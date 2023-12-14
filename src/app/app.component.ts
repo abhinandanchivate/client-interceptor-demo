ca37ffe74d94bb038a91239f247cd6b1f9bfaa9f
abhinandanchivate <abhinandanchivate2000@gmail.com>
2023-12-14 11:37:39 +0530

    updated client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const headerData = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client-interceptor-demo';
+
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.http.get('https://testestset.com/', headerData).subscribe({
+      next: (res) => console.log('success', res),
+      error: (err) => console.error('error', err),
+    });
+  }
 }
